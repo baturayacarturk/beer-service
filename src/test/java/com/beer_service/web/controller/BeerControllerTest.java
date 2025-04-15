db18913c3d9af7243ea13117e2d6c7c4908d9434
Baturay Acarturk <baturayacarturk@outlook.com>
2025-04-16 00:45:00 +0300

    fix failing tests

--- src/test/java/com/beer_service/web/controller/BeerControllerTest.java
+++ src/test/java/com/beer_service/web/controller/BeerControllerTest.java
@@ -1,17 +1,21 @@
 package com.beer_service.web.controller;
 
+import com.beer_service.services.BeerService;
 import com.beer_service.web.model.BeerDto;
 import com.beer_service.web.model.BeerStyleEnum;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -24,9 +28,13 @@ class BeerControllerTest {
 
     @Autowired
     ObjectMapper objectMapper;
+    @MockBean
+    BeerService beerService;
 
     @Test
     public void getBeerById() throws Exception {
+        given(beerService.getById(any())).willReturn(BeerDto.builder().build());
+
         mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
@@ -35,7 +43,7 @@ class BeerControllerTest {
     void saveNewBeer() throws Exception {
         BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
-
+        given(beerService.saveNewBeer(any(BeerDto.class))).willReturn(beerDto);
         mockMvc.perform(post("/api/v1/beer")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(beerDtoJson))
